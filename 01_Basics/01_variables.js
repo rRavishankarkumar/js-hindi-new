ec772dd86dbd60b17248b684d18aba2a67a89ba8
ravishankarkuma <101902193+rRavishankarkumar@users.noreply.github.com>
2023-12-03 06:58:01 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 8521879182
+let accountEmail = "ravishankarkumar852187@gmail.com"
+var accountPassword = "12345"
+accountCity = "Patna"
+let accountState = "Bihar";
+// accountId = 2 // not allowed
+
+{
+
+}
+
+accountEmail = "rv@rs.com"
+accountPassword = "21212"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer version
+
+// alert( 3+3) //we are using node js, not console
+
+console.log(3 + 3)
+
+console.log("Ravishankar")
+
+let name = "Ravishankar"
+let age = 20
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefind =>
+//symbol 
+
+
+//object
+
+console.log(typeof null);
